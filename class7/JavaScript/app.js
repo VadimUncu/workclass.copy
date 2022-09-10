038055b270dd52189a856637117c9223cf8bdef1
VadimUncu <uncu.vadim@yahoo.com>
2022-09-10 15:36:35 +0100

    first commit

---
+++ class7/JavaScript/app.js
@@ -0,0 +1,18 @@
+let number = 70;
+let number1 = 100;
+let TotalNumber = number + number1;
+
+document.getElementById('app').innerHTML = TotalNumber;
+
+
+let car = {
+    brand: "ford",
+    miles: "12000",
+    fuelType: "Diesel"
+};
+
+document.getElementById('app1').innerHTML = car.fuelType;
+
+let fruits = ["Apple", "Orange", "Pear", "Banana"];
+
+alert(fruits);
